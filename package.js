0247d92b5365661405c5782bf181bbbde21991a1
Eslam A. Hefnawy <eahefnawy@gmail.com>
2014-07-11 01:24:18 +0300

    updated package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Reaction Braintree - Braintree Payment Module for Reaction commerce"
 });
 
-Npm.depends({'braintree': '1.14.1'});
+Npm.depends({'braintree': '1.15.0'});
 
 Package.on_use(function (api, where) {
   api.use([
@@ -17,12 +17,15 @@ Package.on_use(function (api, where) {
   ], ["client", "server"]);
 
   api.add_files("common/collections.coffee",["client","server"]);
+  api.add_files("server/braintree.coffee",["server"]);
   api.add_files([
     "client/register.coffee",
     "client/routing.coffee",
 
     "client/templates/braintree.html",
-    "client/templates/braintree.coffee"
+    "client/templates/braintree.coffee",
+    "client/templates/braintreetest/braintreetest.html",
+    "client/templates/braintreetest/braintreetest.coffee"
   ], ["client"]);
   
   api.export([
